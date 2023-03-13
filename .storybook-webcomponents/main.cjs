9d14041c9ebf9dd158b1fd8e0c73fa4fa0bb1be7
Pablo Maldonado <pablomartinmaldonado1@gmail.com>
2023-03-13 14:59:58 -0300

    chore: 🔧 add TailwindCSS

--- .storybook-webcomponents/main.cjs
+++ .storybook-webcomponents/main.cjs
@@ -1,6 +1,18 @@
 module.exports = {
   stories: ['../src/web-components/**/*.stories.mdx', '../src/web-components/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    {
+      name: '@storybook/addon-styling',
+      options: {
+        // Check out https://github.com/storybookjs/addon-styling/blob/main/docs/api.md
+        // For more details on this addon's options.
+        postCss: true,
+      },
+    },
+  ],
   framework: '@storybook/web-components',
   core: {
     builder: '@storybook/builder-vite',
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
+import './index.css'; // Import Tailwind CSS
+
 import './web-components/Button/WebButton';
 import './web-components/Title/WebTitle';
