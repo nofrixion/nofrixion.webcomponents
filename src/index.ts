f080413af6b874f2927e4964c6cd048e63efce1b
Pablo Maldonado <pablomartinmaldonado1@gmail.com>
2023-03-14 08:46:21 -0300

    fix: 🚑️ fix build

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import './index.css'; // Import Tailwind CSS
 
-import './web-components/Button/WebButton';
+// Web Components that want to be built
+// need to be imported here
 import './web-components/Title/WebTitle';
+import './web-components/Card/WebCard';
