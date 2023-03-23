7558651c01f5478588bed4e1dbf13ad89ef27e37
donalnofrixion <89344328+donalnofrixion@users.noreply.github.com>
2023-03-23 15:36:43 +0000

    Added DateRange component

--- src/components/ui/DateRangePicker/DateRangePicker.tsx
+++ src/components/ui/DateRangePicker/DateRangePicker.tsx
@@ -1,13 +1,14 @@
 import classNames from 'classnames';
 import { useEffect, useState } from 'react';
-import { format } from 'date-fns';
 import { dateRanges } from '../../../utils/constants';
-import { getDateFormat, getDateInPast } from '../../../utils/formatters';
+import { getDateInPast } from '../../../utils/formatters';
 import * as DropdownMenu from '@radix-ui/react-dropdown-menu';
 import { motion } from 'framer-motion';
 import { cva } from 'class-variance-authority';
 import { DateRangePicker as DateRange } from 'rsuite';
-import 'rsuite/dist/rsuite.min.css';
+import '../../../rsuite.css';
+
+const pillClasses = 'bg-[#EDF2F7] text-sm whitespace-nowrap border-[1px] border-[#D5DBDD] cursor-pointer';
 
 const actionItemClassNames =
   'group text-xs leading-none rounded-1 flex items-center relative select-none outline-none cursor-pointer py-2';
@@ -15,7 +16,7 @@ const actionItemClassNames =
 const actionItem = cva(actionItemClassNames, {
   variants: {
     intent: {
-      neutral: ['data-[highlighted]:text-greyText'],
+      neutral: ['data-[highlighted]:text-[#00264D]'],
       selected: ['text-highlightedGreenText data-[highlighted]:cursor-default'],
     },
   },
@@ -36,15 +37,8 @@ interface DateRangeFilterProps {
 
 const DateRangePicker = ({ rangeText = dateRanges.last90Days, onDateChange }: DateRangeFilterProps) => {
   const [selectRangeText, setSelectRangeText] = useState(rangeText);
-  const [dateRangeText, setDateRangeText] = useState('');
   const [fromDate, setFromDate] = useState(new Date());
   const [toDate, setToDate] = useState(new Date());
-
-  const pillClasses = 'bg-[#EDF2F7] py-1 text-sm whitespace-nowrap border-[1px] border-[#D5DBDD] cursor-pointer';
-
-  const getDateRangeText = (toDate: Date, fromDate: Date): string => {
-    return `${format(toDate, getDateFormat(toDate, fromDate))} - ${format(fromDate, getDateFormat(toDate, fromDate))}`;
-  };
 
   useEffect(() => {
     onDateChange({ fromDate: fromDate, toDate: toDate });
@@ -55,40 +49,32 @@ const DateRangePicker = ({ rangeText = dateRanges.last90Days, onDateChange }: Da
   }, [rangeText]);
 
   useEffect(() => {
-    let toDate = new Date();
+    let fromDate = new Date();
 
     switch (selectRangeText) {
       case dateRanges.today:
-        setDateRangeText(format(new Date(), 'MMM do'));
-
-        setFromDate(new Date());
-        setToDate(getDateInPast(0, true));
+        setFromDate(getDateInPast(0, true));
+        setToDate(new Date());
         break;
       case dateRanges.yesterday:
-        toDate = getDateInPast(1, true);
-
-        setDateRangeText(format(toDate, 'MMM do'));
-
-        setFromDate(getDateInPast(1, false));
-        setToDate(toDate);
+        fromDate = getDateInPast(1, true);
+        setFromDate(fromDate);
+        setToDate(getDateInPast(1, false));
         break;
       case dateRanges.last7Days:
-        toDate = getDateInPast(7, true);
-        setDateRangeText(getDateRangeText(new Date(), toDate));
-        setFromDate(new Date());
-        setToDate(toDate);
+        fromDate = getDateInPast(7, true);
+        setFromDate(fromDate);
+        setToDate(new Date());
         break;
       case dateRanges.last30Days:
-        toDate = getDateInPast(30, true);
-        setDateRangeText(getDateRangeText(new Date(), toDate));
-        setFromDate(new Date());
-        setToDate(toDate);
+        fromDate = getDateInPast(30, true);
+        setFromDate(fromDate);
+        setToDate(new Date());
         break;
       case dateRanges.last90Days:
-        toDate = getDateInPast(90, true);
-        setDateRangeText(getDateRangeText(new Date(), toDate));
-        setFromDate(new Date());
-        setToDate(toDate);
+        fromDate = getDateInPast(90, true);
+        setFromDate(fromDate);
+        setToDate(new Date());
         break;
     }
   }, [selectRangeText]);
@@ -97,7 +83,12 @@ const DateRangePicker = ({ rangeText = dateRanges.last90Days, onDateChange }: Da
     <div className="flex defaultText">
       <DropdownMenu.Root>
         <DropdownMenu.Trigger asChild>
-          <div className={classNames(pillClasses, 'rounded-l-full border-r-0 flex flex-col-2 space-x-2 px-3')}>
+          <div
+            className={classNames(
+              pillClasses,
+              'text-[#00264D] rounded-l-full border-r-0 flex flex-col-2 space-x-2 pl-4 pr-3',
+            )}
+          >
             <div className="py-2">
               <span>{selectRangeText}</span>
             </div>
@@ -147,37 +138,17 @@ const DateRangePicker = ({ rangeText = dateRanges.last90Days, onDateChange }: Da
           editable={false}
           onOpen={() => console.log('open')}
           format="MMM do, y"
-          // defaultValue={[fromDate, toDate]}
           value={[fromDate, toDate]}
           size="md"
           ranges={[]}
-          onChange={(item) => {
+          onOk={(item) => {
             if (item !== null) {
-              setFromDate(item[1]);
-              setToDate(item[0]);
+              setFromDate(item[0]);
+              setToDate(item[1]);
               setSelectRangeText('Custom');
             }
           }}
         />
-        {/* <div className="py-1">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            width="14"
-            height="14"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5"
-            />
-          </svg>
-        </div>
-        <div>
-          <span>{dateRangeText}</span>
-        </div> */}
       </div>
     </div>
   );
