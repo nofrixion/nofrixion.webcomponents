1fd37523f1359da7889d1de0f79a457a0d02f904
sauravmaiti22 <103389834+sauravmaiti22@users.noreply.github.com>
2023-05-18 14:35:54 +0100

    Covered all cases.

--- src/components/ui/AmountPaid/AmountPaid.tsx
+++ src/components/ui/AmountPaid/AmountPaid.tsx
@@ -7,24 +7,31 @@ const AmountPaid = ({
 }: {
   amountPaid: number;
   totalAmount: number;
-  currency: Currency;
+  currency: Currency.EUR | Currency.GBP;
 }) => {
-  const percentagePaid = (amountPaid / totalAmount) * 100;
+  const percentagePaid = amountPaid > totalAmount ? 100 : (amountPaid / totalAmount) * 100;
+  const outstandingAmount = amountPaid > totalAmount ? 0 : totalAmount - amountPaid;
   return (
     <>
       <div className="flex flex-col">
-        <span className="text-2xl font-semibold leading-8 mb-4 text-[#00264D]">
-          {currency == 'GBP' ? '£' : '€'}
-          {amountPaid.toFixed(2)}
+        <span
+          className={`text-2xl tabular-nums font-semibold leading-8 mb-4  ${
+            amountPaid === 0 ? 'text-[#73808C]' : 'text-[#00264D]'
+          }`}
+        >
+          {currency == Currency.GBP ? '£' : '€'}
+          {amountPaid === 0 ? totalAmount : amountPaid.toFixed(2)}
         </span>
         <div className="flex flex-row h-[0.125rem]">
           <div className="bg-[#40BFBF]" style={{ flexGrow: `${percentagePaid}` }}></div>
           <div className="bg-[#EDF2F7]" style={{ flexGrow: `${100 - percentagePaid}` }}></div>
         </div>
-        <span className="text-sm leading-5 text-[#73808C] font-medium mt-2">
-          {currency == 'GBP' ? '£' : '€'}
-          {(totalAmount - amountPaid).toFixed(2)} outstanding
-        </span>
+        {amountPaid > 0 && (
+          <span className="text-sm leading-5 text-[#73808C] font-medium mt-2">
+            {currency == Currency.GBP ? '£' : '€'}
+            <span className="tabular-nums">{outstandingAmount.toFixed(2)}</span> outstanding
+          </span>
+        )}
       </div>
     </>
   );
