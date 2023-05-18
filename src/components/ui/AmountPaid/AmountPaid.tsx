4ea9c73a28adcb04792a63b3ab3123e69ed27070
sauravmaiti22 <103389834+sauravmaiti22@users.noreply.github.com>
2023-05-18 15:42:33 +0100

    Used existing colours and used classnames

--- src/components/ui/AmountPaid/AmountPaid.tsx
+++ src/components/ui/AmountPaid/AmountPaid.tsx
@@ -1,3 +1,4 @@
+import classNames from 'classnames';
 import { Currency } from '../../../api/types/Enums';
 
 const AmountPaid = ({
@@ -12,28 +13,29 @@ const AmountPaid = ({
   const percentagePaid = amountPaid > totalAmount ? 100 : (amountPaid / totalAmount) * 100;
   const outstandingAmount = amountPaid > totalAmount ? 0 : totalAmount - amountPaid;
   return (
-    <>
-      <div className="flex flex-col">
-        <span
-          className={`text-[2rem] tabular-nums font-semibold leading-8 mb-4  ${
-            amountPaid === 0 ? 'text-[#73808C]' : 'text-[#00264D]'
-          }`}
-        >
-          {currency == Currency.GBP ? '£' : '€'}
-          {amountPaid === 0 ? totalAmount : amountPaid.toFixed(2)}
-        </span>
-        <div className="flex flex-row h-[0.125rem]">
-          <div className="bg-[#40BFBF] rounded-l-sm" style={{ flexGrow: `${percentagePaid}` }}></div>
-          <div className="bg-[#EDF2F7] rounded-r-sm" style={{ flexGrow: `${100 - percentagePaid}` }}></div>
-        </div>
-        {amountPaid > 0 && (
-          <span className="text-sm leading-[1.063rem] text-[#73808C] font-medium mt-2">
-            {currency == Currency.GBP ? '£' : '€'}
-            <span className="tabular-nums">{outstandingAmount.toFixed(2)}</span> outstanding
-          </span>
+    <div className="flex flex-col">
+      <span
+        className={classNames('text-[2rem] tabular-nums font-semibold leading-8 mb-4', {
+          'text-greyText': amountPaid === 0,
+          'text-defaultText': amountPaid > 0,
+        })}
+      >
+        {currency == Currency.GBP ? '£' : '€'}
+        {amountPaid === 0 ? totalAmount : amountPaid.toFixed(2)}
+      </span>
+      <div className="flex flex-row h-[0.125rem]">
+        {percentagePaid !== 0 && (
+          <div className="bg-positiveActionBackground rounded-l-sm" style={{ flexGrow: `${percentagePaid}` }}></div>
         )}
+        <div className="bg-greyBg rounded-r-sm" style={{ flexGrow: `${100 - percentagePaid}` }}></div>
       </div>
-    </>
+      {amountPaid > 0 && (
+        <span className="text-sm leading-[1.063rem] text-greyText font-medium mt-2">
+          {currency == Currency.GBP ? '£' : '€'}
+          <span className="tabular-nums">{outstandingAmount.toFixed(2)}</span> outstanding
+        </span>
+      )}
+    </div>
   );
 };
 
