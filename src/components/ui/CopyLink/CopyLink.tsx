6566135f7d7333dcf792b5a715f0d0ce878b5f94
sauravmaiti22 <103389834+sauravmaiti22@users.noreply.github.com>
2023-05-18 10:00:18 +0100

    Fixed left padding

--- src/components/ui/CopyLink/CopyLink.tsx
+++ src/components/ui/CopyLink/CopyLink.tsx
@@ -20,8 +20,8 @@ export const CopyLink = ({ link }: { link: string }) => {
   };
 
   return (
-    <div className="flex items-center h-10 rounded-full justify-between px-2 pr-4 shadow-[0_0_0.5rem_rgba(4,41,49,0.15)] text-[0.813rem] relative">
-      <div className="whitespace-nowrap overflow-x-clip">{link}</div>
+    <div className="flex items-center h-10 rounded-full justify-between px-4 pr-4 shadow-[0_0_0.5rem_rgba(4,41,49,0.15)] text-[0.813rem] relative">
+      <span className="whitespace-nowrap overflow-x-clip">{link}</span>
       <div className="w-14 h-6 bg-gradient-to-l from-white right-[5.5rem] absolute"></div>
       <AnimatePresence>
         {!copied ? (
