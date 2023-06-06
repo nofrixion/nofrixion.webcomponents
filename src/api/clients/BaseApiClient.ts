fcccf5aad2994176d27631b95b1754f2cfdb0c78
sauravmaiti22 <103389834+sauravmaiti22@users.noreply.github.com>
2023-06-06 16:20:19 +0100

    Using formdata for post and put.

--- src/api/clients/BaseApiClient.ts
+++ src/api/clients/BaseApiClient.ts
@@ -78,7 +78,13 @@ export abstract class BaseApiClient {
   }> {
     console.log(`Requesting: ${method} ${url}`);
 
-    const contentType = 'application/json';
+    let contentType = 'application/json';
+
+    // Send form encoding on POST and PUT
+    // Axios will automatically serialize the postData object to form urlencoded format
+    if (method === HttpMethod.POST || method === HttpMethod.PUT) {
+      contentType = 'application/x-www-form-urlencoded';
+    }
 
     try {
       const { data } = await axios<TResponse>({
